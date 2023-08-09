6b50cc696beb570763c07319b6efed16404ed27d
dvkwhyd <135089471+jyferz33@users.noreply.github.com>
2023-08-09 09:03:51 +0700

    vikiyunani
    vikidewapetir

---
+++ operatorAritmatika.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+public class operatorAritmatika {
+    public static void main(String[] args) {
+        String x = "hasil rapot e viki";
+        int bilangan1= 13;
+        int bilangan2= 5;
+        int hasilpenjumlahan = bilangan1+bilangan2;
+        int hasilpengurangan = bilangan1-bilangan2;
+        int hasilpembagian = bilangan1/bilangan2;
+        int hasilperkalian = bilangan1*bilangan2;
+        int modulus = bilangan1%bilangan2;
+        double mat = 91.5;
+        double rpl = 92.5;
+        double inf = 93.5;
+        double pkn = 94.5;
+        float or = 95.5f;
+        double rtRt = (mat+rpl+inf+pkn+or)/5;
+        double bb = 75.9;
+        double tb = 1.707;
+        double indeks = bb/(tb*tb);
+        System.out.println("BMI = "+indeks);
+//          System.out.println("opoiki = "+x);
+//          System.out.println("bilangan1 = "+bilangan1);
+//          System.out.println("bilangan2 = "+bilangan2);
+//          System.out.println("hasil pengurangan = "+hasilpengurangan);
+//          System.out.println("hasil penjumlahan = "+hasilpenjumlahan);
+//          System.out.println("hasil pembagian = "+hasilpembagian);
+//          System.out.println("hasil perkalian = "+hasilperkalian);
+//          System.out.println("modulus = "+modulus);
+//          System.out.println("Nilai rata rata = "+rtRt);
+          
+          
+          
+          
+          
+          
+          
+          
+          
+    }
+    
+}
